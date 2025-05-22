9fc10d771ca84743ccb8200238298366702002ea
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-22 15:59:51 +0700

    update: Slice Simulator

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -4,18 +4,27 @@ import {
   getSlices,
 } from "@slicemachine/adapter-next/simulator";
 import { SliceZone } from "@prismicio/react";
+import { redirect } from "next/navigation";
 
 import { components } from "@/slices";
 
-export default async function SliceSimulatorPage({
+export default function SliceSimulatorPage({
   searchParams,
-}: SliceSimulatorParams) {
-  const { state } = await searchParams;
-  const slices = getSlices(state);
+}: SliceSimulatorParams & { searchParams: { secret?: string } }) {
+  if (
+    process.env.SLICE_SIMULATOR_SECRET &&
+    searchParams.secret !== process.env.SLICE_SIMULATOR_SECRET
+  ) {
+    redirect("/");
+  }
+
+  const slices = getSlices(searchParams.state);
 
   return (
-    <SliceSimulator>
-      <SliceZone slices={slices} components={components} />
+    <SliceSimulator background="" zIndex={10}>
+      <div className="max-h-[900px]">
+        <SliceZone slices={slices} components={components} />
+      </div>
     </SliceSimulator>
   );
 }
