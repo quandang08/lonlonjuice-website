46f6078654768e5c049006c9039d5825c00a01e0
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-23 20:56:29 +0700

    feat: alternating animation

--- src/slices/AlternatingText/index.tsx
+++ src/slices/AlternatingText/index.tsx
@@ -1,6 +1,15 @@
-import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+"use client";
+
+import { Bounded } from "@/components/Bounded";
+import { asText, Content } from "@prismicio/client";
+import {
+  PrismicRichText,
+  PrismicText,
+  SliceComponentProps,
+} from "@prismicio/react";
+import { View } from "@react-three/drei";
+import Scene from "./Scene";
+import clsx from "clsx";
 
 /**
  * Props for `AlternatingText`.
@@ -11,42 +20,43 @@ export type AlternatingTextProps =
 /**
  * Component for "AlternatingText" Slices.
  */
-const AlternatingText: FC<AlternatingTextProps> = ({ slice }) => {
+const AlternatingText = ({ slice }: AlternatingTextProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="alternating-text-container relative bg-yellow-300 text-sky-950"
     >
-      Placeholder component for alternating_text (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use Prismic MCP with your code editor
-       *
-       * Get AI-powered help to build your slice components — based on your actual model.
-       *
-       * ▶️ Setup:
-       * 1. Add a new MCP Server in your code editor:
-       *
-       * {
-       *   "mcpServers": {
-       *     "Prismic MCP": {
-       *       "command": "npx",
-       *       "args": ["-y", "@prismicio/mcp-server"]
-       *     }
-       *   }
-       * }
-       *
-       * 2. Select Claude 3.7 Sonnet (recommended for optimal output)
-       *
-       * ✅ Then open your slice file and ask your code editor:
-       *    "Code this slice"
-       *
-       * Your code editor reads your slice model and helps you code faster ⚡
-       * 📚 Give your feedback: https://community.prismic.io/t/help-us-shape-the-future-of-slice-creation/19505
-       */}
-    </section>
+      <div>
+        <div className="relative z-[100] grid">
+          <View className="alternating-text-view absolute left-0 top-0 h-screen w-full">
+            <Scene />
+          </View>
+
+          {slice.primary.text_group.map((item, index) => (
+            <div
+              key={asText(item.heading)}
+              className="alternating-section grid h-screen place-items-center gap-x-12 md:grid-cols-2"
+            >
+              <div
+                className={clsx(
+                  index % 2 === 0 ? "col-start-1" : "md:col-start-2",
+
+                  "rounded-lg p-4 backdrop-blur-lg max-md:bg-white/30",
+                )}
+              >
+                <h2 className="text-balance text-6xl font-bold">
+                  <PrismicText field={item.heading} />
+                </h2>
+                <div className="mt-4 text-xl">
+                  <PrismicRichText field={item.body} />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
